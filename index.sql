6efa65cded1ca11bd46f5bdc01e26383efbd5bab
drbarak <drbarak@talkie.co.il>
2022-07-20 19:06:23 +0300

    Initial version

---
+++ index.sql
@@ -0,0 +1,11 @@
+/* can add INDEX when creating a table (can use the keywork KEY instead of INDEX) */
+USE sample_staff;
+CREATE TABLE salary2 (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    amount DECIMAL(11,2) NOT NULL DEFAULT 0,
+    employee_id INT NOT NULL DEFAULT 0,
+    INDEX idx_amount (amount)
+    );
+DESC salary2;
+INSERT INTO salary2(amount, employee_id) VALUES (10000,1);
+SELECT * FROM salary2;
