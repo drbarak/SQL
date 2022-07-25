0770b9047f5fa2296bcb9e0c16faed29938b03f0
drbarak <drbarak@talkie.co.il>
2022-07-25 21:32:45 +0300

    changed names

--- Analytical_functions.sql
+++
@@ -1,9 +0,0 @@
-/*  ---S06-L01----------------  Analytic functions -----------------
-MySQL
-	hmmm... not many, but we can learn to simulate them (yay!)
-	See User-defined functions in C/C++ to enhance MySQL
-PostgreSQL
-	ROW_NUMBER()
-	RANK(), DENSE_RANK()
-	LAG(), `LEAD()``
-	FIRST_VALUE(), LAST_VALUE()
